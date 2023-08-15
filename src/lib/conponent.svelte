c657b77ab1fd29e28ce82c9d3e17db3b003db625
shamokit <contact@shamokit.com>
2023-08-15 23:22:43 +0900

    add comment

--- src/lib/conponent.svelte
+++ src/lib/conponent.svelte
@@ -32,3 +32,41 @@
 		>
 	{/each}
 </select>
+
+<!--
+@component
+
+```html
+  <script lang="ts" strictEvents>
+    import Component from '../lib/conponent.svelte';
+    const libraries = [
+      {
+        value: 'eslint',
+        label: 'ESLint',
+      },
+      {
+        value: 'eslint-plugin-svelte',
+        label: 'eslint-plugin-svelte',
+      },
+    ] as const;
+    const defaultLibrary = 'eslint';
+    let selectedValue = defaultLibrary;
+  </script>
+```
+
+```tsx
+  <h1>Welcome to SvelteKit</h1>
+  <Component
+    libraries="{libraries}"
+    library="{defaultLibrary}"
+    let:favoriteFramework
+    on:chaaaaaaange="{({ detail: { value: _value } }) => (selectedValue = _value)}"
+  >
+    <p>
+      {favoriteFramework}はいいぞ
+    </p>
+    <hr />
+    <p>Selected library is {selectedValue}</p>
+  </Component>
+```
+-->
